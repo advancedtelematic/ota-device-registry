57acfb3d9efed20535e879b77c496dfbaf098135
Ignacio Lucero <ignacio.lucero@here.com>
2018-10-08 12:42:48 +0200

    Support smart groups expression 'deviceid position is not'.

--- src/main/scala/com/advancedtelematic/ota/deviceregistry/data/GroupExpressionParser.scala
+++ src/main/scala/com/advancedtelematic/ota/deviceregistry/data/GroupExpressionParser.scala
@@ -4,6 +4,7 @@ import atto.Atto._
 import atto._
 import cats.data.NonEmptyList
 import cats.syntax.either._
+import com.advancedtelematic.libats.http.Errors.RawError
 import com.advancedtelematic.libats.slick.db.SlickExtensions._
 import com.advancedtelematic.ota.deviceregistry.common.Errors
 import com.advancedtelematic.ota.deviceregistry.data.Group.GroupExpression
@@ -16,6 +17,7 @@ object GroupExpressionAST {
   sealed trait Expression
   case class DeviceIdContains(word: String) extends Expression
   case class DeviceIdCharAt(char: Char, position: Int) extends Expression
+  case class DeviceIdCharAtNot(char: Char, position: Int) extends Expression
   case class Or(cond: NonEmptyList[Expression])  extends Expression
   case class And(cond: NonEmptyList[Expression]) extends Expression
 
@@ -26,8 +28,7 @@ object GroupExpressionAST {
     compileString(groupExpression.value, evalToScala)
 
   private def compileString[T](str: String, fn: Expression => T): T =
-    GroupExpressionParser.parse(str)
-      .map(fn).right.get
+    GroupExpressionParser.parse(str).map(fn).right.get
 
   private def evalToScala(exp: Expression): Device => Boolean = exp match {
     case DeviceIdContains(word) =>
@@ -35,6 +36,9 @@ object GroupExpressionAST {
 
     case DeviceIdCharAt(c, p) =>
       (d: Device) => d.deviceId.exists(id => p < id.underlying.length && id.underlying.charAt(p).toLower == c.toLower)
+
+    case DeviceIdCharAtNot(c, p) =>
+      evalToScala(DeviceIdCharAt(c, p)).andThen(!_)
 
     case Or(conds) =>
       val evaledConds = conds.map(evalToScala)
@@ -52,6 +56,9 @@ object GroupExpressionAST {
     case DeviceIdCharAt(c, p) =>
       (d: DeviceTable) => d.deviceId.mappedTo[String].substring(p, p + 1).toLowerCase.mappedTo[Char] === c.toLower
 
+    case DeviceIdCharAtNot(c, p) =>
+      eval(DeviceIdCharAt(c, p)).andThen(!_)
+
     case Or(conds) =>
       val evaledConds = conds.map(eval)
       evaledConds.reduceLeft { (a, b) => (d: DeviceTable) => a(d) || b(d) }
@@ -63,14 +70,14 @@ object GroupExpressionAST {
 }
 
 object GroupExpressionParser {
-  def parse(str: String) =
+  def parse(str: String): Either[RawError, Expression] =
     parser.parse(str).done.either.leftMap(Errors.InvalidGroupExpression)
 
   private lazy val expression: Parser[Expression] = or | and | leftExpression
 
   private lazy val leftExpression: Parser[Expression] = deviceIdExpression | brackets
 
-  private lazy val deviceIdExpression: Parser[Expression] = deviceIdCons ~> (deviceIdContains |  deviceIdCharAt)
+  private lazy val deviceIdExpression: Parser[Expression] = deviceIdCons ~> (deviceIdContains | deviceIdCharAtIsOrIsNot)
 
   private lazy val brackets: Parser[Expression] = parens(expression)
 
@@ -83,13 +90,17 @@ object GroupExpressionParser {
     str <- takeWhile1(c => c.isLetterOrDigit || c == '-')
   } yield DeviceIdContains(str)
 
-  private lazy val deviceIdCharAt: Parser[Expression] = for {
+  private lazy val deviceIdCharAtIsOrIsNot: Parser[Expression] = for {
     _ <- string("position")
     pos <- parens(int.filter(_ > 0))
     _ <- skipWhitespace
     _ <- token(string("is"))
+    not <- opt(token(string("not")))
     char <- letterOrDigit
-  } yield DeviceIdCharAt(char, pos - 1)
+  } yield not match {
+      case Some(_) => DeviceIdCharAtNot(char, pos - 1)
+      case None    => DeviceIdCharAt(char, pos - 1)
+  }
 
   private lazy val and: Parser[Expression] = for {
     a <- leftExpression
